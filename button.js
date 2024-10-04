dcbb80778996392a550e3ccdac0a2e11cde6f3ad
Jeevitha K <jeevideepa21@gmail.com>
2024-10-04 16:25:34 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,21 @@
+function changeRed(){
+    const red=document.getElementById('red');
+    document.body.style.backgroundColor = "red";
+}
+function changeBlue(){
+    const blue=document.getElementById('blue');
+    document.body.style.backgroundColor = "blue";
+}
+function changeGreen(){
+    const green=document.getElementById('green');
+    document.body.style.backgroundColor = "green";
+}
+function reset(){
+    const red=document.getElementById('red');
+    document.body.style.backgroundColor = "";
+    const blue=document.getElementById('blue');
+    document.body.style.backgroundColor = "";
+    const green=document.getElementById('green');
+    document.body.style.backgroundColor = "";
+
+}
